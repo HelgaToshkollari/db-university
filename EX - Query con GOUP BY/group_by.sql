ae504682d26ddb7a8044b4b683eda6b084c388d2
HelgaToshkollari <toshkollari.helga@gmail.com>
2023-01-12 18:32:29 +0100

    adds group_by

---
+++ EX - Query con GOUP BY/group_by.sql
@@ -0,0 +1,27 @@
+1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT COUNT(*) as `iscritti` , YEAR(`enrolment_date`) as `anno_iscrizione` 
+FROM `students`
+GROUP BY `anno_iscrizione`;
+
+
+2. Contare gli insegnanti che hanno lufficio nello stesso edificio
+
+SELECT COUNT(*) as `insegnanti`, `office_address`as `edificio`
+FROM `teachers`
+GROUP BY `edificio`;
+
+
+3. Calcolare la media dei voti di ogni appello desame
+
+SELECT COUNT(*) as `esame`, AVG(`vote`) as `media_voti`, `exam_id` 
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+
+
+4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT COUNT(*) as `numero_corsi_di_laurea` , `department_id` as 			`dipartimento`
+FROM `degrees`
+GROUP BY `dipartimento`;
